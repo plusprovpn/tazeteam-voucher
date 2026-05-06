c6985fb36e1620ee01dd979bec1f2b69738ad89e
plusprovpn <tazeteams3@gmail.com>
2026-05-06 14:34:36 +0630

    Update install.py

--- install.py
+++ install.py
@@ -2,15 +2,15 @@ import urllib.request
 import importlib.util
 from pathlib import Path
 
-SO_URL = "https://raw.githubusercontent.com/plusprovpn/tazeteam-voucher/main/taze.so"
-SO_FILE = "taze.so"
+SO_URL = "https://raw.githubusercontent.com/plusprovpn/tazeteam-voucher/main/tazeteam.so"
+SO_FILE = "tazeteam.so"
 
 print("[*] Downloading core...")
 urllib.request.urlretrieve(SO_URL, SO_FILE)
 print("[+] Download complete")
 
 p = Path(SO_FILE).resolve()
-spec = importlib.util.spec_from_file_location("taze", str(p))
+spec = importlib.util.spec_from_file_location("tazeteam", str(p))
 mod = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(mod)
 
