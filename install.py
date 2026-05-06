0b583fac4610bd12ff088899ebadd1606f15d503
plusprovpn <tazeteams3@gmail.com>
2026-05-06 12:46:51 +0630

    Update install.py

--- install.py
+++ install.py
@@ -5,15 +5,8 @@ import subprocess
 CORE_URL = "https://raw.githubusercontent.com/plusprovpn/tazeteam-voucher/main/ruijie_core.py"
 CORE_FILE = "ruijie_core.py"
 
-def main():
-try:
 print("[*] Downloading core...")
 urllib.request.urlretrieve(CORE_URL, CORE_FILE)
 print("[+] Download complete")
 print("[*] Running...")
 subprocess.run([sys.executable, CORE_FILE], check=True)
-except Exception as e:
-print(f"[!] Error: {e}")
-
-if __name__ == "__main__":
-main()
